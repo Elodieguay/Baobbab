ebb3dd1329048976dfdf53dac7174fa3a9c8f9ea
Elodieguay <e.guayroso@gmail.com>
2025-05-12 00:39:40 +0200

    fix migration categories bugs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
