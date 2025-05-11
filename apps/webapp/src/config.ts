00a90b277ed87d65435f754961d005f552dd3aa6
Elodieguay <e.guayroso@gmail.com>
2025-05-11 16:47:25 +0200

    fix seeders

--- apps/webapp/src/config.ts
+++ apps/webapp/src/config.ts
@@ -1,3 +1,6 @@
+const defaultPort = import.meta.env.VITE_API_PORT || '5000';
+const defaultUrl = `http://localhost:${defaultPort}`;
+
 export const config = {
-    apiUrl: import.meta.env.VITE_API_URL ?? 'http://localhost:3000',
+    apiUrl: import.meta.env.VITE_API_URL || defaultUrl,
 };
