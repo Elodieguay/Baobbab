e2427775031b268ad86d176b2d93c4a923b56a6a
Elodieguay <e.guayroso@gmail.com>
2025-05-21 01:22:45 +0200

    🐛 bug

--- apps/api/start.sh
+++ apps/api/start.sh
@@ -3,9 +3,9 @@
 echo "Running migrations..."
 pnpm --filter api mikro-orm migration:up
 
-echo "Migrations applied."
-echo "Running seeders..."
-pnpm --filter api mikro-orm seeder:run
+# echo "Migrations applied."
+# echo "Running seeders..."
+# pnpm --filter api mikro-orm seeder:run
 
 echo "Starting the server..."
 node apps/api/dist/main.js
